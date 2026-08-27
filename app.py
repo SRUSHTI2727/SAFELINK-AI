b3751722f85816c89948f5cbc62e403f765f9eb5
Srushti Kalkutaki <srushtikalkutaki245@gmail.com>
2026-08-27 15:11:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if not st.session_state.show_scanner:
         <style>
         .welcome-container {
             text-align: center;
-            padding: 15px 20px 20px 20px;
+            padding: 15px 15px 15px 15px;
         }
 
         .welcome-shield {
